3733e1a0c87e071462ce14d3b43b80b3c44dab82
duane-faria <duane.faria@etec.sp.gov.br>
2020-11-07 19:25:04 -0300

    Resolvendo um bug ao informar o token nas requisições http

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:3000/',
   baseURL: process.env.VUE_APP_API_URL,
 });
 
 api.interceptors.request.use(
   (config) => {
+    if (!window.localStorage.vuex) {
+      return config;
+    }
     const { user } = JSON.parse(window.localStorage.vuex);
     if (user && user.token) {
       config.headers.Authorization = `Bearer ${user.token}`;
